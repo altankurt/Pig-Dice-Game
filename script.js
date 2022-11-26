bcd9c58db1bc3fa4cb14aa922765224ca6ab85e0
altankurt <aaltankurt@gmail.com>
2022-11-26 23:40:53 +0300

    selecting elements

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// selecting elements
+const diceSt = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
