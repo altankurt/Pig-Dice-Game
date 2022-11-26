3a4908307c8aa2a13d11d6d1013533153f14c735
altankurt <aaltankurt@gmail.com>
2022-11-27 02:48:16 +0300

    fix: invalid space character in "finish game"

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ btnHold.addEventListener('click', function () {
 
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add(' player--winner');
+        .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
